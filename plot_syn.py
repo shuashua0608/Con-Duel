f9d8df2f4087b3f73d56a194eaec5cb95497dcb4
shuaz <74279976+shuashua0608@users.noreply.github.com>
2023-04-21 15:56:12 +0800

    Update plot_syn.py

--- plot_syn.py
+++ plot_syn.py
@@ -18,40 +18,24 @@ import time
 from functools import partial
 from matplotlib.pyplot import MultipleLocator
 import matplotlib.colors as mcolors
-
+pth1 = ""
+pth2 = "
 ####
 algo_names = ["Rconucb-PosNeg","Rconucb-Diff","Random_opt","MaxInp", "ConDuel-random", "ConDuel-maxinp", "ConDuel-bs"]
-syn_sum = np.load("/data/ysh/project_uai/data/synthetic/rebuttal/syn_linsum1.npy", allow_pickle=True)
-syn = np.load("/data/ysh/project_uai/data/synthetic/rebuttal/syn_lin1.npy", allow_pickle=True)
+syn_sum = np.load(pth1, allow_pickle=True)
+syn = np.load(pth2, allow_pickle=True)
 _,_,regret_std = sum_users(syn, algo_names)
 ###
 
-syn1_sum = np.load("/data/ysh/project_uai/data/synthetic/rebuttal_new/linsum1.npy", allow_pickle=True)
-syn1 = np.load("/data/ysh/project_uai/data/synthetic/rebuttal_new/lin1.npy", allow_pickle=True)
-
-#
-
-
 MEDIUM_SIZE = 10
 BIGGER_SIZE = 14
-# "ConDuel-random", "ConDuel-maxinp",
-#algo_names = ["ConDuel-random", "ConDuel-maxinp", "ConDuel-bs", "MaxInp", "Random_opt","Rconucb-PosNeg","Rconucb-Diff"]
 
-#algo_names_select = ["ConDuel-bs", "Rconucb-PosNeg", "Rconucb-Diff", "MaxInp"]
-# reg_new
-#algo_names_select = ["ConDuel-bs", "Rconucb-PosNeg", "Rconucb-Diff", "MaxInp"]
-# reg_new
+
 #algo_names_select =  ["ConDuel-random", "ConDuel-maxinp", "ConDuel-bs"]
 algo_names_select =  algo_names
 reg_syn1 = syn1_sum[0]
-r0 = reg_syn1["ConDuel-random"]
-r1 = reg_syn1["ConDuel-bs"]
-#reg_syn1["ConDuel-bs"] = r0
-#reg_syn1["ConDuel-random"] = r1
 
-reg_syn1["MaxInp"] = syn_sum[0]["MaxInp"]
-reg_syn1["Random_opt"] = syn_sum[0]["Random_opt"]
-print(reg_syn1)
+
 
 plt.rc('axes', titlesize=BIGGER_SIZE)  # fontsize of the title
 plt.rc('axes', labelsize=BIGGER_SIZE)  # fontsize of the x and y labels
@@ -72,8 +56,8 @@ colors = list(mcolors.TABLEAU_COLORS.keys())
 
 for i, algoname in enumerate(algo_names_select):
     plt.plot(reg_syn1[algoname], label=algo_names_select[i], color=colors[i])
-    #plt.fill_between(range(len(reg_syn1[algoname])),(reg_syn1[algoname] - regret_std[algoname]).clip(0, None),
-                  # reg_syn1[algoname] + regret_std[algoname], color=colors[i], alpha=0.2)
+    plt.fill_between(range(len(reg_syn1[algoname])),(reg_syn1[algoname] - regret_std[algoname]).clip(0, None),
+                      reg_syn1[algoname] + regret_std[algoname], color=colors[i], alpha=0.2)
 
 plt.xlabel('Iterations')
 plt.ylabel("Regret")
